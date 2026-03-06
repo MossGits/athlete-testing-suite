899777da559a5cc31579e866f33ff3d174778d17
MossGits <102268373+MossGits@users.noreply.github.com>
2026-03-06 10:26:35 -0500

    Update TestSessionRepo.java

--- backend/src/main/java/com/example/athletebackend/repo/TestSessionRepo.java
+++ backend/src/main/java/com/example/athletebackend/repo/TestSessionRepo.java
@@ -3,6 +3,9 @@ package com.example.athletebackend.repo;
 import com.example.athletebackend.model.TestSession;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
-public interface TestSessionRepo extends JpaRepository<TestSession, UUID> {}
+public interface TestSessionRepo extends JpaRepository<TestSession, UUID> {
+    List<TestSession> findAllByAthleteRefOrderByCreatedAtDesc(String athleteRef);
+}
